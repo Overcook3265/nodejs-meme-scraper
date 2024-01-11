1553e729d76e71c2ef8290336142805d8f166e03
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-11 16:44:18 +0100

    Add first working image downloader

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 import axios from 'axios';
 import extractUrls from 'extract-urls';
+import fs from 'fs';
+import https from 'https';
 
 const webSite = await axios(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
 
 const websiteHtml = webSite.data;
-// console.log(websiteHtml.substring(1, 10000));
 
 const urls = extractUrls(websiteHtml);
-//console.log(urls);
 
 const picUrls = urls.filter(function (str) {
   return str.includes(
     'https://memecomplete.com/edit/https://api.memegen.link/images/',
   );
 });
-console.log(picUrls);
-console.log(picUrls.length);
+const firstPicUrls = picUrls.slice(0, 10);
+console.log(firstPicUrls);
+
+//===working image downloader===
+// const stream = fs.createWriteStream('01.jpg');
+// https.get(
+//   'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg',
+//   function (response) {
+//     response.pipe(stream);
+//   },
+// );
+
+// const downloadURI = (uri, name) => {
+//   const link = document.createElement('a');
+//   link.download = name;
+//   link.href = uri;
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+// };
+// downloadURI(
+//   'https://memecomplete.com/edit/https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg',
+//   '01.jpg',
+// );
+
+// const file = new File(['foo'], 'new-note.txt', {
+//   type: 'text/plain',
+// });
